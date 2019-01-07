c14aa739b38d688707ad986754e5521a7d47fb0f
DudeCalledZee <40342826+DudeCalledZee@users.noreply.github.com>
2019-01-07 17:10:04 +0000

    random phrases working

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const qwerty = document.querySelector('#qwerty');
 const score = document.querySelectorAll('.tries');
 const overlay = document.getElementById('overlay');
-
 let missed = 0;
-
-
 const phrases = [
   'Cattle prod',
   'oyster ditch',
@@ -13,7 +10,22 @@ const phrases = [
   'cranny axe',
   'gut locker',
 ];
+const PhraseArray = getRandomPhraseAsArray(phrases);
 
 overlay.addEventListener('click',() => {
   overlay.style.display = 'none';
 });
+
+
+function getRandomPhraseAsArray(arr) {
+  let randomPhrase = Math.floor(Math.random() * phrases.length);
+  let word = arr[randomPhrase].toLowerCase();
+  return word.split('');
+}
+
+function addPhraseToDisplay(arr){
+    // do stuff any arr that is passed in, and add to `#phrase ul`
+}
+
+
+addPhrasetoDisplay(phraseArray);

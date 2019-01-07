92cb0b7847b3aaee0577a13234065af1cbc685ee
DudeCalledZee <40342826+DudeCalledZee@users.noreply.github.com>
2019-01-07 16:13:55 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const qwerty = document.querySelector('#qwerty');
+const score = document.querySelectorAll('.tries');
+const overLay = document.getElementById('overlay');
+
+let missed = 0;
+
+
+const phrases = [
+  'Cattle prod',
+  'oyster ditch',
+  'lap rocket',
+  'Batter dip',
+  'cranny axe',
+  'gut locker',
+];

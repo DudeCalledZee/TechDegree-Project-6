cf4fae4eba4b7912373cf388717191ce0eb25a9c
DudeCalledZee <40342826+DudeCalledZee@users.noreply.github.com>
2019-01-10 15:49:16 +0000

    adds check letter and shows on display

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const qwerty = document.querySelector('#qwerty');
 const score = document.querySelectorAll('.tries');
 const overlay = document.getElementById('overlay');
-
+let letter;
 let missed = 0;
 const phrases = [
   'Cattle prod',
@@ -22,7 +22,7 @@ function getRandomPhraseAsArray(arr) {
   let randomPhrase = Math.floor(Math.random() * phrases.length);
   let words = arr[randomPhrase].toLowerCase();
   return words.split('');
-}
+};
 
 function addPhraseToDisplay(arr) {
   for (i = 0; i < arr.length; i += 1) {
@@ -36,7 +36,7 @@ function addPhraseToDisplay(arr) {
       item.className = "letter";
     }
   }
-}
+};
 
 addPhraseToDisplay(phraseArray);
 
@@ -44,4 +44,28 @@ addPhraseToDisplay(phraseArray);
 let keypress = qwerty.addEventListener('click', function(event) {
   keypress = event.target.textContent;
   letter = JSON.stringify(keypress);
+});
+
+
+
+
+function checkLetter(selectLetter) {
+  const hidenLetter = document.querySelectorAll('.letter');
+  let letterFound = false;
+  for (let i = 0; i < hidenLetter.length; i++) {
+    if (selectLetter === hidenLetter[i].textContent) {
+      hidenLetter[i].classList.add('show');
+      letterFound = true;
+    }
+  }
+  return letterFound;
+}
+
+qwerty.addEventListener('click', (e) => {
+  if (e.target.tagName === 'BUTTON') {
+    let chosenLetter = e.target;
+    chosenLetter.classList.add('chosen');
+    chosenLetter.setAttribute('disabled', '');
+    let letterFound = checkLetter(chosenLetter.textContent);
+  }
 });
